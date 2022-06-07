44e47a737ec638ca87f66577c2754c749cfccb90
3Y3G <3Y3G@DESKTOP-UVREEQA>
2022-06-07 20:18:20 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelDesktopPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(435, 375);
+            this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // panelDesktopPanel
+            // 
+            this.panelDesktopPanel.BackColor = System.Drawing.Color.Black;
+            this.panelDesktopPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktopPanel.Location = new System.Drawing.Point(0, 0);
+            this.panelDesktopPanel.Name = "panelDesktopPanel";
+            this.panelDesktopPanel.Size = new System.Drawing.Size(800, 450);
+            this.panelDesktopPanel.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 45);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(236, 393);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.panelDesktopPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Panel panelDesktopPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int buttonInt = 0;
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+         (
+               int nLeftRect,
+               int nTopRect,
+               int nRightRect,
+               int nBottomRect,
+               int nWidthEllipse,
+               int nHeightEllipse
+
+         );
+        public static Form1 form1Instance;
+        private Form activeForm;
+        private Button currentButton;
+        public Panel panel123;
+
+        public void Kekw()
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello");
+                Thread.Sleep(1000);
+                flowLayoutPanel2.BringToFront();
+
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel123 = panelDesktopPanel;
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    currentButton = (Button)btnSender;
+
+                }
+            }
+        }
+        private void DisableButton()
+        {
+
+        }
+        public void OpenForm1()
+        {
+            
+            this.panelDesktopPanel.Size = new Size(0, 0);
+            panelDesktopPanel.SendToBack();
+            panelDesktopPanel.SendToBack();
+            flowLayoutPanel2.BringToFront();
+            button1.BringToFront();
+            //Kekw();
+        }
+        public void OpenChildForm2(Form childFrom)
+        {
+
+            panelDesktopPanel.BringToFront();
+
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+
+            activeForm = childFrom;
+            childFrom.BringToFront();
+            childFrom.TopLevel = false;
+            childFrom.FormBorderStyle = FormBorderStyle.None;
+
+            this.panelDesktopPanel.Controls.Add(childFrom);
+            this.panelDesktopPanel.Tag = childFrom;
+            childFrom.BringToFront();
+            childFrom.Show();
+            button1.BringToFront();
+        }
+        public void OpenChildForm(Form childFrom)
+        {
+            
+            panelDesktopPanel.BringToFront();
+     
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            
+            activeForm = childFrom;
+            childFrom.BringToFront();
+            childFrom.TopLevel = false;
+            childFrom.FormBorderStyle = FormBorderStyle.None;
+    
+            this.panelDesktopPanel.Controls.Add(childFrom);
+            this.panelDesktopPanel.Tag = childFrom;
+            childFrom.BringToFront();
+            childFrom.Show();
+            button1.BringToFront();
+        }
+        public void CloseChildForm(Form childForm)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (buttonInt == 0)
+            {
+                
+                OpenChildForm(new Form2());
+                button1.Text = "Close";
+                buttonInt = 1;
+            }
+            else if (buttonInt == 1)
+            {
+                buttonInt = 0;
+                OpenChildForm(new Form1());
+                button1.Text = "Open";
+            }
+            
+        }
+    }
+}
